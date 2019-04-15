934b7ff7a74671b3ba60bde326e164bee63a2006
Andre Marcelo-Tanner <andre@enthropia.com>
2019-04-15 19:08:44 +0800

    Add /conversations and /conversations/{conversation_id} handlers

--- internal/webserver/webserver.go
+++ internal/webserver/webserver.go
@@ -18,6 +18,16 @@ import (
 type Route struct {
 	URL         string `json:"url,omitempty"`
 	Description string `json:"description,omitempty"`
+	Methods     string `json:"methods,omitempty"`
+	Handler     func(w http.ResponseWriter, r *http.Request)
+}
+
+type Conversation struct {
+	ID             int
+	ConversationID string
+	Sender         string
+	Message        string
+	CreatedDate    string
 }
 
 // parse templates dir
@@ -32,16 +42,17 @@ const webPort = 8000
 func Start(mysqlConfig config.DbConfig) {
 	var err error
 
-	routes = append(routes, Route{URL: "/", Description: "Index"})
-	routes = append(routes, Route{URL: "/conversations/{conversation_id}", Description: "View a Conversation"})
-	routes = append(routes, Route{URL: "/messages", Description: "Message handler"})
+	routes = append(routes, Route{URL: "/", Description: "Homepage", Methods: "GET", Handler: GetIndex})
+	routes = append(routes, Route{URL: "/conversations", Description: "Show conversations", Methods: "GET", Handler: GetConversationList})
+	routes = append(routes, Route{URL: "/conversations/{conversation_id}", Description: "View a Conversation", Methods: "GET", Handler: GetConversation})
+	routes = append(routes, Route{URL: "/messages", Description: "Message handler", Methods: "POST", Handler: PostMessages})
 
 	router := mux.NewRouter()
 	router.StrictSlash(true)
 
-	router.HandleFunc("/", GetIndex).Methods("GET")
-	router.HandleFunc("/conversations/{conversation_id}", GetConversation).Methods("GET")
-	router.HandleFunc("/messages", PostMessages).Methods("POST")
+	for _, route := range routes {
+		router.HandleFunc(route.URL, route.Handler).Methods(route.Methods)
+	}
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./web/static"))).Methods("GET")
 
 	dbConnectionString := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v", mysqlConfig.Username, mysqlConfig.Password, mysqlConfig.Host, mysqlConfig.Port, mysqlConfig.DBName)
@@ -67,23 +78,110 @@ func Start(mysqlConfig config.DbConfig) {
 // GetIndex shows the homepage
 func GetIndex(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	err := templates.ExecuteTemplate(w, "index.html", nil)
+	err := templates.ExecuteTemplate(w, "index.html", routes)
 	if err != nil {
-		renderError(w, "EXECUTE_TEMPLATE", http.StatusInternalServerError)
+		renderError(w, "EXECUTE_TEMPLATE_ERROR", http.StatusInternalServerError)
+		log.Println(err)
+	}
+}
+
+// GetConversationList shows the conversation
+func GetConversationList(w http.ResponseWriter, r *http.Request) {
+	var err error
+
+	rows, err := db.Query("SELECT DISTINCT `conversation_id` FROM `conversations`")
+	if err != nil {
+		renderError(w, "ERROR_DB_QUERY", http.StatusInternalServerError)
+		log.Println(err)
+
+		return
+	}
+	defer rows.Close()
+
+	conversations := []string{}
+	for rows.Next() {
+		var conversationID string
+		err = rows.Scan(&conversationID)
+		if err != nil {
+			switch err {
+			case sql.ErrNoRows:
+				renderError(w, "ERROR_NOT_FOUND", http.StatusNotFound)
+			default:
+				renderError(w, "ERROR_ROWS_SCAN", http.StatusInternalServerError)
+			}
+			log.Println(err)
+			return
+		}
+		conversations = append(conversations, conversationID)
+	}
+	err = rows.Err()
+	if err != nil {
+		renderError(w, "ERROR_ROWS_NEXT", http.StatusInternalServerError)
+		log.Println(err)
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	err = templates.ExecuteTemplate(w, "conversation_list.html", conversations)
+	if err != nil {
+		renderError(w, "EXECUTE_TEMPLATE_ERROR", http.StatusInternalServerError)
 		log.Println(err)
 	}
 }
 
 // GetConversation shows the conversation
 func GetConversation(w http.ResponseWriter, r *http.Request) {
+	var err error
 	vars := mux.Vars(r)
 
-	conversation := vars["conversation_id"]
+	conversationID := vars["conversation_id"]
+	rows, err := db.Query("SELECT `id`, `conversation_id`, `sender`, `message`, `created_date` FROM `conversations` WHERE `conversation_id` = ?", conversationID)
+	if err != nil {
+		renderError(w, "ERROR_DB_QUERY", http.StatusInternalServerError)
+		log.Println(err)
+
+		return
+	}
+	defer rows.Close()
+
+	conversations := []Conversation{}
+	for rows.Next() {
+		var c Conversation
+		err = rows.Scan(&c.ID, &c.ConversationID, &c.Sender, &c.Message, &c.CreatedDate)
+		if err != nil {
+			renderError(w, "ERROR_ROWS_SCAN", http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
+		conversations = append(conversations, c)
+	}
+	err = rows.Err()
+	if err != nil {
+		renderError(w, "ERROR_ROWS_NEXT", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	if len(conversations) == 0 {
+		renderError(w, "CONVERSATION_NOT_FOUND", http.StatusNotFound)
+		log.Println("Invalid Conversation ID")
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
-	err := templates.ExecuteTemplate(w, "conversation.html", conversation)
+
+	data := struct {
+		ConversationID string
+		Conversations  []Conversation
+	}{
+		ConversationID: conversationID,
+		Conversations:  conversations,
+	}
+	err = templates.ExecuteTemplate(w, "conversation.html", data)
 	if err != nil {
-		renderError(w, "EXECUTE_TEMPLATE", http.StatusInternalServerError)
+		renderError(w, "EXECUTE_TEMPLATE_ERROR", http.StatusInternalServerError)
 		log.Println(err)
 	}
 }
@@ -99,6 +197,7 @@ func PostMessages(w http.ResponseWriter, r *http.Request) {
 		"sender",
 		"message")
 	_, err = db.Query(insertQuery)
+
 	// if there is an error inserting, handle it
 	if err != nil {
 		renderError(w, "CANT_INSERT_DB", http.StatusInternalServerError)
